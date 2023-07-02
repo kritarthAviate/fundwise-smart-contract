0fbf16a3519404ea9840d4db60d092ec13f4e992
Alien Coder <paidcodecamp@gmail.com>
2023-07-02 13:02:11 +0530

    wip: simlation script

--- scripts/simulate.js
+++ scripts/simulate.js
@@ -8,11 +8,12 @@ const { ethers, network } = require("hardhat");
 const { getProxyAddress } = require("../utils");
 const ProxyContractABI =
     require("../artifacts/contracts/fundWithEther/CrowdfundingWithEth.sol/CrowdfundingWithEth.json").abi;
+const IERC20ABI = require("../artifacts/@openzeppelin/contracts/token/ERC20/IERC20.sol/IERC20.json").abi;
 
 async function main() {
     console.log("------Simulating deployment of CrowdfundingWithEth and CrowdfundingFactoryContract------");
     const { AAVE_V2_ADDRESS, AAVE_ATOKEN_ADDRESS, LENDING_POOL_PROVIDER_ADDRESS } = network.config.constants;
-    const [deployer, signer1, signer2] = await ethers.getSigners();
+    const [deployer, signer1, signer2, signer3, signer4, signer5, signer6, signer7] = await ethers.getSigners();
     console.log({
         deployer: deployer.address,
         signer1: signer1.address,
@@ -45,7 +46,7 @@ async function main() {
 
     const txnForEthProxy = await crowdfundingFactoryContract
         .connect(signer1)
-        .createFund(1, 2, "ipfs_Ka_Hash", signer2.address);
+        .createFund(1, ethers.utils.parseEther("100"), "ipfs_Ka_Hash", signer2.address);
     const receiptForEthProxy = await txnForEthProxy.wait();
     const proxyAddress = getProxyAddress(receiptForEthProxy);
     const proxyContract = new ethers.Contract(proxyAddress, ProxyContractABI, deployer);
@@ -54,6 +55,47 @@ async function main() {
         owner: await proxyContract.creatorAddress(),
         beneficiary: await proxyContract.receiver(),
     });
+
+    console.log("------Contributing to the fund from signer3------");
+    const txnForContribution = await proxyContract.connect(signer3).contribute({ value: ethers.utils.parseEther("1") });
+
+    console.log("------Contributing to the fund from signer4------");
+    const txnForContribution2 = await proxyContract
+        .connect(signer4)
+        .contribute({ value: ethers.utils.parseEther("10") });
+
+    console.log("------Contributing to the fund from signer5------");
+    const txnForContribution3 = await proxyContract
+        .connect(signer5)
+        .contribute({ value: ethers.utils.parseEther("5") });
+
+    console.log("------Contributing to the fund from signer6------");
+    const txnForContribution4 = await proxyContract
+        .connect(signer6)
+        .contribute({ value: ethers.utils.parseEther("9") });
+
+    console.log("------Contributing to the fund from signer7------");
+    const txnForContribution5 = await proxyContract
+        .connect(signer7)
+        .contribute({ value: ethers.utils.parseEther("80") });
+
+    // init aave aToken contract
+    const aTokenContract = new ethers.Contract(AAVE_ATOKEN_ADDRESS, IERC20ABI, deployer);
+    // get aave aToken balance of proxy contract
+    const aTokenBalanceOfProxy = await aTokenContract.balanceOf(proxyContract.address);
+
+    console.log({
+        amountRaised: ethers.utils.formatEther(await proxyContract.amountRaised()).toString(),
+        projectStatus: await proxyContract.projectStatus(),
+        aTokenBalanceOfProxy: ethers.utils.formatEther(aTokenBalanceOfProxy.toString()),
+    });
+
+    console.log("------Withdrawing funds from signer2------");
+    const signer2BalanceBefore = await signer2.getBalance();
+    console.log({ signer2BalanceBefore: ethers.utils.formatEther(signer2BalanceBefore.toString()) });
+    const txnForWithdrawal = await proxyContract.connect(signer2).withdrawFunds();
+    const signer2BalanceAfter = await signer2.getBalance();
+    console.log({ signer2BalanceAfter: ethers.utils.formatEther(signer2BalanceAfter.toString()) });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
